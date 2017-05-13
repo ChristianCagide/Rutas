fa7db198fc3b93c2783dbf569117427d48aabfdf
ChristianCagide <ccagide1@gmail.com>
2017-05-13 10:07:51 -0500

    Rutas

---
+++ Rutas/proyControlInv/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace proyControlInv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReporte = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.txtReporte = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.btnAgregarInicio = new System.Windows.Forms.Button();
+            this.btnReporteInverso = new System.Windows.Forms.Button();
+            this.btnEliminarInicio = new System.Windows.Forms.Button();
+            this.btnEliminarUlt = new System.Windows.Forms.Button();
+            this.txtNombreD = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRecorrido = new System.Windows.Forms.Button();
+            this.txtHoraFin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHoraInicio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnReporte
+            // 
+            this.btnReporte.Location = new System.Drawing.Point(197, 197);
+            this.btnReporte.Name = "btnReporte";
+            this.btnReporte.Size = new System.Drawing.Size(75, 23);
+            this.btnReporte.TabIndex = 0;
+            this.btnReporte.Text = "Reporte";
+            this.btnReporte.UseVisualStyleBackColor = true;
+            this.btnReporte.Click += new System.EventHandler(this.btnReporte_Click);
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(10, 168);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(91, 168);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(10, 197);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(66, 32);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(66, 58);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 6;
+            // 
+            // txtReporte
+            // 
+            this.txtReporte.Location = new System.Drawing.Point(172, 32);
+            this.txtReporte.Multiline = true;
+            this.txtReporte.Name = "txtReporte";
+            this.txtReporte.Size = new System.Drawing.Size(100, 159);
+            this.txtReporte.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Minutos";
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(91, 197);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertar.TabIndex = 13;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // btnAgregarInicio
+            // 
+            this.btnAgregarInicio.Location = new System.Drawing.Point(10, 226);
+            this.btnAgregarInicio.Name = "btnAgregarInicio";
+            this.btnAgregarInicio.Size = new System.Drawing.Size(75, 36);
+            this.btnAgregarInicio.TabIndex = 16;
+            this.btnAgregarInicio.Text = "Agregar en Inicio";
+            this.btnAgregarInicio.UseVisualStyleBackColor = true;
+            this.btnAgregarInicio.Click += new System.EventHandler(this.btnAgregarInicio_Click);
+            // 
+            // btnReporteInverso
+            // 
+            this.btnReporteInverso.Location = new System.Drawing.Point(197, 226);
+            this.btnReporteInverso.Name = "btnReporteInverso";
+            this.btnReporteInverso.Size = new System.Drawing.Size(75, 36);
+            this.btnReporteInverso.TabIndex = 17;
+            this.btnReporteInverso.Text = "Reporte Inverso";
+            this.btnReporteInverso.UseVisualStyleBackColor = true;
+            this.btnReporteInverso.Click += new System.EventHandler(this.btnReporteInverso_Click);
+            // 
+            // btnEliminarInicio
+            // 
+            this.btnEliminarInicio.Location = new System.Drawing.Point(91, 226);
+            this.btnEliminarInicio.Name = "btnEliminarInicio";
+            this.btnEliminarInicio.Size = new System.Drawing.Size(75, 36);
+            this.btnEliminarInicio.TabIndex = 18;
+            this.btnEliminarInicio.Text = "Eliminar Inicio";
+            this.btnEliminarInicio.UseVisualStyleBackColor = true;
+            this.btnEliminarInicio.Click += new System.EventHandler(this.btnEliminarInicio_Click);
+            // 
+            // btnEliminarUlt
+            // 
+            this.btnEliminarUlt.Location = new System.Drawing.Point(10, 268);
+            this.btnEliminarUlt.Name = "btnEliminarUlt";
+            this.btnEliminarUlt.Size = new System.Drawing.Size(75, 36);
+            this.btnEliminarUlt.TabIndex = 19;
+            this.btnEliminarUlt.Text = "Eliminar Ultimo";
+            this.btnEliminarUlt.UseVisualStyleBackColor = true;
+            this.btnEliminarUlt.Click += new System.EventHandler(this.btnEliminarUlt_Click);
+            // 
+            // txtNombreD
+            // 
+            this.txtNombreD.Location = new System.Drawing.Point(66, 84);
+            this.txtNombreD.Name = "txtNombreD";
+            this.txtNombreD.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreD.TabIndex = 20;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "NombreD";
+            // 
+            // btnRecorrido
+            // 
+            this.btnRecorrido.Location = new System.Drawing.Point(197, 268);
+            this.btnRecorrido.Name = "btnRecorrido";
+            this.btnRecorrido.Size = new System.Drawing.Size(75, 36);
+            this.btnRecorrido.TabIndex = 22;
+            this.btnRecorrido.Text = "Recorrido";
+            this.btnRecorrido.UseVisualStyleBackColor = true;
+            this.btnRecorrido.Click += new System.EventHandler(this.btnRecorrido_Click);
+            // 
+            // txtHoraFin
+            // 
+            this.txtHoraFin.Location = new System.Drawing.Point(66, 110);
+            this.txtHoraFin.Name = "txtHoraFin";
+            this.txtHoraFin.Size = new System.Drawing.Size(100, 20);
+            this.txtHoraFin.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 24;
+            this.label4.Text = "Hora Fin";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 26;
+            this.label5.Text = "Hora Inicio";
+            // 
+            // txtHoraInicio
+            // 
+            this.txtHoraInicio.Location = new System.Drawing.Point(76, 136);
+            this.txtHoraInicio.Name = "txtHoraInicio";
+            this.txtHoraInicio.Size = new System.Drawing.Size(90, 20);
+            this.txtHoraInicio.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 312);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHoraInicio);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtHoraFin);
+            this.Controls.Add(this.btnRecorrido);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreD);
+            this.Controls.Add(this.btnEliminarUlt);
+            this.Controls.Add(this.btnEliminarInicio);
+            this.Controls.Add(this.btnReporteInverso);
+            this.Controls.Add(this.btnAgregarInicio);
+            this.Controls.Add(this.btnInsertar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtReporte);
+            this.Controls.Add(this.txtCantidad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnReporte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReporte;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.TextBox txtReporte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertar;
+        private System.Windows.Forms.Button btnAgregarInicio;
+        private System.Windows.Forms.Button btnReporteInverso;
+        private System.Windows.Forms.Button btnEliminarInicio;
+        private System.Windows.Forms.Button btnEliminarUlt;
+        private System.Windows.Forms.TextBox txtNombreD;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRecorrido;
+        private System.Windows.Forms.TextBox txtHoraFin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHoraInicio;
+    }
+}
+
